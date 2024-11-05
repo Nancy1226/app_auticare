a1f22155ba0d66ebc35292ac75312191edbc418f
FranEscobarG <escobar.gutierrez.3@gmail.com>
2024-11-05 13:17:58 -0600

    [ADD] Integracion de Donaciones

--- lib/Widgets/app_routes.dart
+++ lib/Widgets/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:app_auticare/Views/home.dart'; // Asegúrate de importar tus vistas
 import 'package:app_auticare/Views/chatbot.dart';
+import 'package:app_auticare/Views/donation.dart';
 import 'package:app_auticare/Authtentication/login.dart';
 
 class AppRoutes {
@@ -9,21 +10,25 @@ class AppRoutes {
   static const String gridView = '/grid';
   static const String chat = '/chat';
   static const String profile = '/profile';
+  static const String donation = '/donation';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
-  switch (settings.name) {
-    case login: 
-      return MaterialPageRoute(builder: (_) => LoginScreen());
-    case home:
-      return MaterialPageRoute(builder: (_) => Home());
-    // case gridView:
-    //   return MaterialPageRoute(builder: (_) => GridViewScreen());
-    case chat:
-      return MaterialPageRoute(builder: (_) => Chatbot());
-    // case profile:
-    //   return MaterialPageRoute(builder: (_) => ProfileScreen());
-    default:
-      throw Exception('Ruta no definida: ${settings.name}'); // Lanzar una excepción para rutas no definidas
+    switch (settings.name) {
+      case login:
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+      case home:
+        return MaterialPageRoute(builder: (_) => Home());
+      // case gridView:
+      //   return MaterialPageRoute(builder: (_) => GridViewScreen());
+      case chat:
+        return MaterialPageRoute(builder: (_) => Chatbot());
+      case donation:
+        return MaterialPageRoute(builder: (_) => Donation());
+      // case profile:
+      //   return MaterialPageRoute(builder: (_) => ProfileScreen());
+      default:
+        throw Exception(
+            'Ruta no definida: ${settings.name}'); // Lanzar una excepción para rutas no definidas
     }
   }
 }
